e28035811edb5dd81780c8e9c7c8cf66c6e8a1dd
Rezki Nasrullah <rezkinasrullah22@gmail.com>
2024-07-01 18:55:24 +0700

    Add site analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,64 +1,61 @@
-import '@sangpencerah/styles/globals.css';
-import { Metadata } from 'next';
+import "@sangpencerah/styles/globals.css";
+import { Metadata } from "next";
 
-import { siteConfig } from '@sangpencerah/config/site';
-import { fontSans } from '@sangpencerah/lib/fonts';
-import { cn } from '@sangpencerah/lib/utils';
-import SiteHeader from '@sangpencerah/components/site-header';
-import { TailwindIndicator } from '@sangpencerah/components/tailwind-indicator';
-import { ThemeProvider } from '@sangpencerah/components/theme-provider';
-import { getLocale, getMessages } from 'next-intl/server';
-import { NextIntlClientProvider } from 'next-intl';
-
+import { siteConfig } from "@sangpencerah/config/site";
+import { fontSans } from "@sangpencerah/lib/fonts";
+import { cn } from "@sangpencerah/lib/utils";
+import SiteHeader from "@sangpencerah/components/site-header";
+import { TailwindIndicator } from "@sangpencerah/components/tailwind-indicator";
+import { ThemeProvider } from "@sangpencerah/components/theme-provider";
+import { getLocale, getMessages } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
-    title: {
-        default: siteConfig.name,
-        template: `%s - ${siteConfig.name}`,
-    },
-    description: siteConfig.description,
-    themeColor: [
-        { media: '(prefers-color-scheme: light)', color: 'white' },
-        { media: '(prefers-color-scheme: dark)', color: 'black' },
-    ],
-    icons: {
-        icon: '/favicon.ico',
-        shortcut: '/favicon-16x16.png',
-        apple: '/apple-touch-icon.png',
-    },
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 interface RootLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export default async function RootLayout({ children }: RootLayoutProps) {
-    const locale = await getLocale();
+  const locale = await getLocale();
 
-    // Providing all messages to the client
-    // side is the easiest way to get started
-    const messages = await getMessages();
-    return (
-        <>
-            <html lang={locale} suppressHydrationWarning>
-                <head />
-                <body
-                    className={cn(
-                        'min-h-screen bg-background font-sans antialiased',
-                        fontSans.variable,
-                    )}
-                >
-                    <NextIntlClientProvider messages={messages}>
-                        <ThemeProvider
-                            attribute="class"
-                            defaultTheme="system"
-                            enableSystem
-                        >
-                            {children}
-                            <TailwindIndicator />
-                        </ThemeProvider>
-                    </NextIntlClientProvider>
-                </body>
-            </html>
-        </>
-    );
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+  return (
+    <>
+      <html lang={locale} suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <NextIntlClientProvider messages={messages}>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              {children}
+              <Analytics />
+              <TailwindIndicator />
+            </ThemeProvider>
+          </NextIntlClientProvider>
+        </body>
+      </html>
+    </>
+  );
 }
