6fd9b91ba80daebde11db80df471388e557f8481
Rezki Nasrullah <rezkinasrullah22@gmail.com>
2024-07-03 21:34:02 +0700

    Debugging vercel response

--- src/app/ai/chat/route.ts
+++ src/app/ai/chat/route.ts
@@ -13,18 +13,23 @@ const openai = createOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+export async function POST(req: Request, resp: any) {
+  try {
+    // Extract the `messages` from the body of the request
+    const { messages } = await req.json();
 
-  // Ask OpenAI for a streaming chat completion given the prompt
+    // Ask OpenAI for a streaming chat completion given the prompt
 
-  const { textStream } = await streamText({
-    model: openai("gpt-3.5-turbo", {}),
-    prompt: messages,
-    system: `You are problem solver, psikolog, a friend, a parent, or someone who user can trust. When user input something, find their problem then find in hadith and quran. So when user input something related or solution in qur'an or on hadith. The hadith book is limited to book hadith of Muslim, Bukhari, Tirmidzi, Nasai, Abu Daud, Ibnu Majah, Imam Ahmad, Darimi, Imam Malik. The hadith must be related to the quran output. Give response based on their language. Give sharia based on salaf sharia. `,
-  });
-  // Convert the response into a friendly text-stream
-  // Respond with the stream
-  return new StreamingTextResponse(textStream);
+    const { textStream } = await streamText({
+      model: openai("gpt-3.5-turbo"),
+      prompt: messages,
+      system: `You are problem solver, psikolog, a friend, a parent, or someone who user can trust. When user input something, find their problem then find in hadith and quran. So when user input something related or solution in qur'an or on hadith. The hadith book is limited to book hadith of Muslim, Bukhari, Tirmidzi, Nasai, Abu Daud, Ibnu Majah, Imam Ahmad, Darimi, Imam Malik. The hadith must be related to the quran output. Give response based on their language. Give sharia based on salaf sharia. `,
+    });
+    // Convert the response into a friendly text-stream
+    // Respond with the stream
+    return new StreamingTextResponse(textStream);
+  } catch (e) {
+    console.error(e);
+    return resp.send(400).send(e);
+  }
 }
